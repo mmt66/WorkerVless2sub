c542ec466e8c6bd0fb8bb5a0e666e303e5b859d3
mmt66 <56261475+mmt66@users.noreply.github.com>
2024-08-07 20:51:39 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -37,7 +37,7 @@ let subconfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/conf
 let noTLS = 'false'; //改为 true , 将不做域名判断 始终返回noTLS节点
 let link = '';
 let edgetunnel = 'ed';
-let RproxyIP = 'false';
+let RproxyIP = 'ture'; //false,ture
 let proxyIPs = [//无法匹配到节点名就随机分配以下ProxyIP域名
 	'31898.cmliu.cloudns.org.cdn.cloudflare.net',
 	'14061.cmliu.cloudns.org.cdn.cloudflare.net',
